0e11398aba12b020d20bedb6b11e1d04c0ef0837
John Esteban Alvarez Piedrahita <esteban.ea145@gmail.com>
2022-06-08 13:33:12 -0500

    add UsuarioRolRepository.java

--- src/main/java/com/crud/democrud/controllers/UsuarioRolController.java
+++ src/main/java/com/crud/democrud/controllers/UsuarioRolController.java
@@ -1,7 +1,26 @@
 package com.crud.democrud.controllers;
 
 
-import com.crud.democrud.models.UsuarioModel;
+/*
+* Haga lo siguiente:
+- agregue un endPoint de actualizacion de usuario
+- incorpore una nueva entidad/modelo(UsuarioRol) que tenga
+  los campos (idUsuario,idRol,Rol("vendedor","cajero","consultor",etc..))
+  un usuario puede en un momento tener 1 o mas roles asociados.
+  debe haber una relacion (Usuario - UsuarioRol):
+        @ManyToOne Relation
+        @OneToMany Relation
+        @OneToOne Relation
+        @ManyToMany Relation
+  aplique la que usted considere mas apropiada y en README
+  justifique ademas incorpore en este mismo README coloque
+varias ScreenShoot(capturas) de la ejecucion de la API REST.
+
+- implemente algunas pruebas unitarias a esa nueva entidad/modelo
+
+Debe registrar en la tabla de abajo su nombre completo y su repositorio
+de github con la solucion.
+* */
 import com.crud.democrud.models.UsuarioRolModel;
 import com.crud.democrud.services.iface.UsuarioRolService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,8 +49,8 @@ public class UsuarioRolController {
     }
 
     @GetMapping(path = "/{id}")
-    public Optional<UsuarioModel> getById(@PathVariable("id") int id){
-        usuarioRolService.getById(id);
+    public Optional<UsuarioRolModel> getById(@PathVariable("id") int id){
+        return usuarioRolService.getById(id);
     }
     
     @PutMapping("/{id}")
